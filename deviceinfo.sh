e03938c38817f31c1bf9c43c4899ff1d10be3291
bikee1235 <bikee.bihari@sstsinc.com>
2024-10-04 13:10:27 +0530

    Create deviceinfo.sh

---
+++ deviceinfo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+#For installing integrated service in all the devices at once
+
+echo -e " Rbox Details......."
+echo -e ${RED} RBOX  IP : ${NC} `ifconfig | grep 10.8.8 | awk '{print $2}'`
+
+if [ $# -eq 1 ]
+then
+CM=$1
+else
+CM=`idevice_id -l`
+fi
+
+ARRAY=($CM)
+for deviceid in "${ARRAY[@]}"
+do
+  echo -e "${RED} UDID  $deviceid${NC} "
+  echo -e ${RED} OS Version : `ideviceinfo -u $deviceid -k ProductVersion` ${NC}
+
+  echo "--------------------Done-----------------------------------"
+done
+
