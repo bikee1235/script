3c177bc6435d467228f1848ddda69cc4e55c2a4d
bikee1235 <bikee.bihari@sstsinc.com>
2024-10-08 10:59:55 +0530

    Update deviceinfo.sh

--- deviceinfo.sh
+++ deviceinfo.sh
@@ -17,6 +17,7 @@ do
   echo -e ${RED} iOS Version : `ideviceinfo -u $deviceid -k ProductVersion` ${NC}
   echo -e ${RED} iOS SerialNumber : `ideviceinfo -u $deviceid -k SerialNumber` ${NC}
   echo -e ${RED} iOS ProductName : `ideviceinfo -u $deviceid -k ProductName` ${NC}
+   echo -e ${RED} ProductType: `ideviceinfo -u $deviceid -k ProductType` ${NC}
   echo -e ${RED} iOS HardwareModel : `ideviceinfo -u $deviceid -k HardwareModel` ${NC}
   echo -e ${RED} Password: `ideviceinfo -u $deviceid -k PasswordProtected` ${NC}
   ps -aef | grep $deviceid
